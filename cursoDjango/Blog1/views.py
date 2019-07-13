bc7944e8359c8e55f0d37ce39e01e76c4fc6eb9b
Your Name <you@example.com>
2019-07-13 18:09:22 +0200

    'test'

--- cursoDjango/Blog1/views.py
+++ cursoDjango/Blog1/views.py
@@ -1,3 +1,10 @@
+from datetime import timedelta
+
 from django.shortcuts import render
+from . import models
+from django.utils import timezone
 
 # Create your views here.
+def post_list(request):
+    post = models.Post.objects.filter(created_date__lte=timezone.now(),created_date__gte=(timezone.now() - timedelta(hours=6)))
+    return render(request, 'Blog1/post_list.html', {"post":post})
